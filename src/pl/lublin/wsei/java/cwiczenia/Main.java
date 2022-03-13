a2747b34eb8e9763e8a652106b2d189211f186f5
irminagorecka <irmina.gorecka@gmail.com>
2022-03-13 16:46:19 +0100

    Initial commit

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,20 @@
+package pl.lublin.wsei.java.cwiczenia;
+/** @param */
+
+public class Main {
+
+    public static void main(String[] args) { //komentarz 1
+	/*
+	k
+	o
+	m
+	e
+	n
+	t
+	 a
+	 r
+	 z
+	 2
+	 */
+    }
+}
