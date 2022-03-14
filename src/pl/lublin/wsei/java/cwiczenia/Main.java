c85436dfa9874c671c9f88fd35a259de04e9b0fe
irminagorecka <irmina.gorecka@gmail.com>
2022-03-14 22:29:33 +0100

    aktualizacja punkty 6-7

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -24,13 +24,29 @@ public class Main {
 	 2
 	 */
 
-        System.out.println("Hejka");
+        System.out.print("Ala\n");
+        System.out.print("ma\n");
+        System.out.print("kota\n");
 
+        System.out.println("Ala");
+        System.out.println("ma");
+        System.out.println("kota");
 
+        int a=333;
+        double b=4.21;
+        String s="tekst";
+        System.out.printf("a=%5d, b=%.2f, s=%10s %n", a,b,s); //%d lub %s to ilość znaków po = , %f to ilość znaków po .
+        System.out.println("Nazwaliśmy to \"witaminą B3\"");
 
+        System.out.printf("alfa\tsin(alfa)\n");
+        for (int i = 0; i <370;i+=10){
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        }
 
-
-
+        System.out.printf("arg\tlog2(arg)\tsum(arg)\n");
+        for (int i = 0; i <10 ;i++){
+            System.out.printf("%d\t%f\t\n", i, Math.log(i));
+        }
 
     }
 
